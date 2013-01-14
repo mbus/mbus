4c904866ccfc403a77001b32ac22f3545f819c9b
Ye-Sheng Kuo <samkuo@umich.edu>
2013-01-14 22:42:00 +0000

    ulpb bit-streaming working version

--- v2/node/ahb_int.v
+++ v2/node/ahb_int.v
@@ -6,6 +6,8 @@ module ahb_int(	HSEL, HADDR, HWRITE, HSIZE, HBURST, HPROT, HTRANS,
 				TX_FAIL, TX_SUCCESS, 
 				fifo_empty, req_out_to_lc);
 
+parameter ADDRESS = 8'hab;
+
 // IOs for AHB bus
 input 			HSEL, HWRITE, HMASTLOCK, HREADY, HRESETn, HCLK;
 input 	[31:0] 	HADDR, HWDATA;
@@ -33,10 +35,10 @@ reg				HREADYOUT;
 reg		[31:0]	HWDATA_REG;
 
 assign HRESP		= 2'b00;
-parameter ADDRESS = 8'hab;
 
 reg				req_in_from_lc;
 wire			ack_out_to_lc;
+reg				ack_out_to_lc_reg;
 wire	[31:0]	DATA_OUT;
 wire			req_out_to_lc;
 reg				ack_in_from_lc;
@@ -58,9 +60,13 @@ begin
 		HWDATA_REG <= 0;
 		TX_ACK <= 0;
 		fifo_re <= 0;
+		req_in_from_lc <= 0;
+		ack_out_to_lc_reg <= 0;
 	end
 	else
 	begin
+		ack_out_to_lc_reg <= ack_out_to_lc;
+
 		if ((TX_SUCCESS | TX_FAIL) & (~TX_ACK))
 			TX_ACK <= 1;
 
@@ -84,46 +90,53 @@ begin
 				if (hwrite_reg)
 				begin
 					HWDATA_REG <= HWDATA;
+					req_in_from_lc <= 1;
+					fsm <= 5;
 				end
 				else
 				begin
 					fifo_re <= 1;
+					fsm <= 2;
 				end
-				fsm <= 2;
 			end
 
 			2:
 			begin
 				fifo_re <= 0;
-				if (hwrite_reg)
-					req_in_from_lc <= 1;
 			   	fsm <= 3;
 			end
 
 			3:
 			begin
-				if (req_in_from_lc)
-				begin
-					if (~ack_out_to_lc)
-					begin
-						req_in_from_lc <= 0;
-						fsm <= 0;
-						HREADYOUT <= 1;
-					end
-				end
-				else
-				begin
-					fsm <= 4;
-				end
+				fsm <= 4;
 			end
 
 			4:
 			begin
 				HRDATA <= fifo_dout;
+				fsm <= 7;
+			end
+
+			5:
+			begin
+				if (ack_out_to_lc_reg)
+				begin
+					req_in_from_lc <= 0;
+					fsm <= 6;
+				end
+			end
+
+			6:
+			begin
+				if (~ack_out_to_lc_reg)
+					fsm <= 7;
+			end
+
+			7:
+			begin
 				fsm <= 0;
 				HREADYOUT <= 1;
 			end
-
 
 		endcase
 	end
@@ -139,16 +152,16 @@ begin
 	else
 	begin
 		if (fifo_we)
-		begin
 			fifo_we <= 0;
-			ack_in_from_lc <= 1;
-		end
 
 		if (ack_in_from_lc&(~req_out_to_lc))
 			ack_in_from_lc <= 0;
 
-		if (req_out_to_lc & (~fifo_full))
+		if (req_out_to_lc & (~fifo_full) & (~ack_in_from_lc) )
+		begin
 			fifo_we <= 1;
+			ack_in_from_lc <= 1;
+		end
 
 	end
 end
@@ -158,7 +171,8 @@ ulpb_node_top #(.ADDRESS(ADDRESS)) n0 (.CLK(SCLK), .RESET(HRESETn), .DIN(DIN), .
 				.ADDR_OUT(), .DATA_OUT(DATA_OUT), .REQ_OUT_TO_LC(req_out_to_lc), .ACK_IN_FROM_LC(ack_in_from_lc), 
 				.TX_FAIL(TX_FAIL), .TX_SUCCESS(TX_SUCCESS), .TX_ACK(TX_ACK));
 
-fifo8x32 rxfifo(
+fifo8x32 #(.WIDTH(32), .DEPTH(8)) rxfifo(
+	.RESET(HRESETn),
 	.DATA(DATA_OUT),
 	.Q(fifo_dout),
 	.CLK(HCLK),

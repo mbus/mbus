0427197e14746c5903bd8fed43693e1207afc723
Ye-Sheng Kuo <samkuo@umich.edu>
2013-02-24 03:38:05 +0000

    update tcl file

--- ulpb_v2/syn/ulpb_ctrl.tcl
+++ ulpb_v2/syn/ulpb_ctrl.tcl
@@ -18,41 +18,12 @@ list_designs
 # Set clock names, ports
 source -verbose "ulpb_ctrl_timing.tcl"
 
-# Register retiming
-#set_balance_registers
-
-# Link the design
-link
-
-# Set maximum fanout of gates
-set_max_fanout 5 $top_level 
-
-# Configure the clock network
-set_fix_hold [all_clocks] 
-set_dont_touch_network $clk_port 
-
-# Set delays: Input, Output
-set_driving_cell -lib_cell INVX2TR [all_inputs]
-#set_driving_cell -lib_cell INVX2TR [all_inputs] -library typical
-set_input_delay $typical_input_delay [all_inputs] -clock $clk_name 
-remove_input_delay -clock $clk_name [find port $clk_port]
-set_output_delay $typical_output_delay [all_outputs] -clock $clk_name 
-
-# Set loading of outputs 
-set_load $typical_wire_load [all_outputs] 
-
-# Set clock
-set_ideal_network $clk_name 
-
-# Verify the design
-check_design
-
-# Uniquify repeated modules
-uniquify
+# Read common settings
+source -verbose "ulpb_node_common_settings.tcl"
 
 # Synthesize the design
 #compile -map_effort medium
-compile_ultra -map_effort medium
+compile_ultra
 
 # Rename modules, signals according to the naming rules
 # Used for tool exchange

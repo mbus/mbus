ffeda1b64dec4de801f10fa4865d0036b7c14f81
Ye-Sheng Kuo <samkuo@umich.edu>
2013-04-17 15:23:48 +0000

    minor update for clock sim

--- mbus/verilog/mbus_clk_sim.v
+++ mbus/verilog/mbus_clk_sim.v
@@ -2,16 +2,14 @@
 module mbus_clk_sim(
 	input	ext_clk,	// generated from testbench, always ticking
 	input	clk_en,		// from sleep controller
-	output	clk_out		// to sleep controller, mbus
+	output	reg clk_out	// to sleep controller, mbus
 );
 
 //assign clk_out = #5 (clk_en&ext_clk);
 
-reg clk_out;
-
 always @ *
 begin
-	if( clk_en)
+	if(clk_en)
 		#3 clk_out <= #3 ext_clk;
 	else
 		#3 clk_out <= #3 1'b0;

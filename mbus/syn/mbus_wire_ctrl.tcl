c13d90c27b7f3dc356b62470cb6e3bfa4684366f
ZhiYoong Foo <zhiyoong@umich.edu>
2013-09-05 19:58:30 +0000

    wire_ctrl.tcl fix

--- mbus/syn/mbus_wire_ctrl.tcl
+++ mbus/syn/mbus_wire_ctrl.tcl
@@ -36,14 +36,9 @@ link
 # Set maximum fanout of gates
 set_max_fanout 4 $top_level 
 
-# Configure the clock network
-set_fix_hold [all_clocks] 
-set_dont_touch_network $clk_port
-
 # Set delays: Input, Output
 set_driving_cell -lib_cell INVX2 [all_inputs]
 set_input_delay $typical_input_delay [all_inputs] -clock $clk_name 
-remove_input_delay -clock $clk_name [find port $clk_port]
 set_output_delay $typical_output_delay [all_outputs] -clock $clk_name 
 
 # Set loading of outputs 

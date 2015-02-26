39e71d57fdce4d463919334293f61e210d7a83b0
Ye-Sheng Kuo <samkuo@umich.edu>
2015-02-26 20:37:26 +0000

    update configuration

--- mbus/verilog/include/layer_ctrl_def.v
+++ mbus/verilog/include/layer_ctrl_def.v
@@ -1,4 +1,13 @@
 
+// Only three configurations are supported
+// 1. Both INT and MEM are enabled
+// 2. Only INT is enabled
+// 3. Both are disabled
+
 `define LC_INT_ENABLE
 `define LC_MEM_ENABLE
+
+// WARNING!!! WARNING!!!
+// DO NOT CHANGE THE FOLLOWING LINES
 `define LC_RF_DATA_WIDTH	24
+`define LC_RF_ADDR_WIDTH	8
